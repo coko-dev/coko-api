634b3e55d8cd4bd78dfcd767c6324e432386ad55
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-09-28 13:31:45 +0900

    Schema definition with ridgepole. Production environment error correction

---
+++ db/Schemafile.rb
@@ -0,0 +1,6 @@
+create_table 'recipe_categories', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
+  t.string 'name'
+  t.string 'name_slug'
+  t.bigint 'recipe_category_id'
+  t.timestamps
+end

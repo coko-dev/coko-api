8764a853c9871146fb987994c70cca887f450826
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-10-01 23:58:54 +0900

    Add models/recipe_category_spec.rb and do it

---
+++ spec/models/recipe_category_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RecipeCategory, type: :model do
+  describe 'test' do
+    context 'when test' do
+      it 'test' do
+        expect(1 * 1).to eq 1
+      end
+    end
+  end
+end

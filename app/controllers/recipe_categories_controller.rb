0d6b64b064959ac99a91ea02a45ab0eee866c201
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-10-10 16:32:53 +0900

    Add Creating recipe category

--- app/controllers/recipe_categories_controller.rb
+++ app/controllers/recipe_categories_controller.rb
@@ -11,4 +11,20 @@ class RecipeCategoriesController < ApplicationController
     recipe_categories = RecipeCategory.all
     render json: { recipe_categories: recipe_categories }
   end
+
+  api :POST, '/recipe_category', 'Create recipe category'
+  def create
+    recipe_category = RecipeCategory.new(recipe_category_params)
+    if recipe_category.save
+      render json: { status: 'SUCCESS', data: recipe_category }
+    else
+      render json: { status: 'ERROR', data: recipe_category.error }
+    end
+  end
+
+  def recipe_category_params
+    params.permit(
+      %i[name name_slug recipe_category_id]
+    )
+  end
 end

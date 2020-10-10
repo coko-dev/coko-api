37dbd9f6f9b48b50fe909c55b01337ecb067c233
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-10-10 15:14:24 +0900

    Add recipe_categories index

--- app/controllers/recipe_categories_controller.rb
+++ app/controllers/recipe_categories_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class RecipeCategoriesController < ApplicationController
-  api :GET, '/', 'Show the recipe category'
+  api :GET, '/', 'Nothing'
   def root
     render json: { status: 'SUCCESS', message: 'API Upgrade token' }
+  end
+
+  api :GET, '/recipe_categories', 'Show the recipe category'
+  def index
+    recipe_categories = RecipeCategory.all
+    render json: { recipe_categories: recipe_categories }
   end
 end

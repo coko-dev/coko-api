f9312c485e594deed4f2e93d3e020725eac3e954
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2020-10-19 10:19:07 +0900

    Add admin/ and screens (#15)

---
+++ app/controllers/admin/recipe_categories_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Admin
+  class RecipeCategoriesController < ApplicationController
+    api :GET, '/admin/recipe_categories', 'Show the recipe category'
+    def index
+      recipe_categories = RecipeCategory.all
+      render json: { recipe_categories: recipe_categories }
+    end
+
+    api :POST, '/admin/recipe_categories', 'Create recipe categories'
+    def create
+      recipe_category = RecipeCategory.new(recipe_category_params)
+      if recipe_category.save
+        render json: { status: 'SUCCESS', data: recipe_category }
+      else
+        render json: { status: 'ERROR', data: recipe_category.error }
+      end
+    end
+
+    def recipe_category_params
+      params.require(:recipe_category).permit(
+        :name,
+        :name_slug,
+        :recipe_category_id
+      )
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   namespace 'v1' do
     resources :recipe_categories, only: %i[index create]
   end
+
+  namespace 'admin' do
+    resources :recipe_categories
+  end
 end

80354d3f8ad654ee42c54716055065f663206b2a
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-10-31 21:31:08 +0900

    Create hot_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_one :user_profile, dependent: :destroy
 
+  has_many :hot_users, dependent: :delete_all
   has_many :followings, foreign_key: 'user_id_from', class_name: 'UserFollow', inverse_of: 'following_user', dependent: :delete_all
   has_many :followers, foreign_key: 'user_id_to', class_name: 'UserFollow', inverse_of: 'follower_user', dependent: :delete_all
   has_many :following_users, through: :followings

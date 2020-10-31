22318ef6d6866888419a1b32973368c0b9a99eab
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-10-31 22:02:28 +0900

    fix typo and change relation name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_one :user_profile, dependent: :destroy
+  has_one :profile, class_name: 'UserProfile', dependent: :destroy
 
   has_many :hot_users, dependent: :delete_all
   has_many :followings, foreign_key: 'user_id_from', class_name: 'UserFollow', inverse_of: 'following_user', dependent: :delete_all
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class UserProfile < ApplicationReocrd
+class UserProfile < ApplicationRecord
   belongs_to :user
 end

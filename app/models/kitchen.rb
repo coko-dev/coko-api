1b9561b1e5932aa921dceee3a37cb734bbcb8d94
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-11-01 12:40:43 +0900

    create owner relation

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Kitchen < ApplicationRecord
+  belongs_to :owner, foreign_key: 'owner_user_id', class_name: 'User', inverse_of: :own_kitchen
+
   has_many :kitchen_joins, dependent: :delete_all
   has_many :kitchen_ocr_histories, dependent: :delete_all
   has_many :kitchen_products, dependent: :delete_all

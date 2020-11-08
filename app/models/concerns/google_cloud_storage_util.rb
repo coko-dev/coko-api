8011add0b32382bc6bc34c82fa02898c3762ba07
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-11-08 21:43:16 +0900

    Add method (delete image & upload image)

--- app/models/concerns/google_cloud_storage_util.rb
+++ app/models/concerns/google_cloud_storage_util.rb
@@ -5,10 +5,6 @@ require 'google/cloud/storage'
 module GoogleCloudStorageUtil
   extend ActiveSupport::Concern
 
-  def fetch_bucket
-    'ff'
-  end
-
   module ClassMethods
     def fetch_bucket
       storage = Google::Cloud::Storage.new(
@@ -16,6 +12,17 @@ module GoogleCloudStorageUtil
         credentials: 'config/coko-prd-bb136db46046.json'
       )
       storage.bucket 'coko_bucket'
+    end
+
+    def delete_image(bucket, file_name, folder_path: 'test/')
+      path = "#{folder_path}#{file_name}"
+      file = bucket.file(path)
+      file.delete if file.present?
+    end
+
+    def upload_image(bucket, image, file_name, folder_path: 'test/')
+      path = "#{folder_path}#{file_name}"
+      bucket.create_file(image.tempfile, path)
     end
   end
 end
--- app/models/hot_recipe.rb
+++ app/models/hot_recipe.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class HotRecipe < ApplicationRecord
+  include GoogleCloudStorageUtil
+
   belongs_to :recipe
+
+  class << self
+    def test
+      fetch_bucket
+    end
+  end
 end

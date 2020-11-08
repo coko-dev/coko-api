1babb04a8cec36cec760e88f7fea53d632ae41d5
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-11-08 09:21:04 +0900

    add module

--- app/models/concerns/google_cloud_storage_util.rb
+++ app/models/concerns/google_cloud_storage_util.rb
@@ -3,11 +3,19 @@
 require 'google/cloud/storage'
 
 module GoogleCloudStorageUtil
+  extend ActiveSupport::Concern
+
   def fetch_bucket
-    storage = Google::Cloud::Storage.new(
-      project_id: 'coko-prd',
-      credentials: 'config/coko-prd-bb136db46046.json'
-    )
-    storage.bucket 'coko_bucket'
+    'ff'
+  end
+
+  module ClassMethods
+    def fetch_bucket
+      storage = Google::Cloud::Storage.new(
+        project_id: 'coko-prd',
+        credentials: 'config/coko-prd-bb136db46046.json'
+      )
+      storage.bucket 'coko_bucket'
+    end
   end
 end

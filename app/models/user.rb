9ad94b7c68fa63f5017b4d573b40fa9f58e64ad9
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-11-21 17:08:26 +0900

    Fix var name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   }
 
   before_validation :set_code, on: :create
-  before_save :set_default_kitchen
+  before_create :set_default_kitchen
 
   # NOTE: OAuth完了時に登録するのでメッセージなし.profilesとは別
   # TODO: 更新時用にメッセージ用意
@@ -41,12 +41,12 @@ class User < ApplicationRecord
     return if self[:code].present?
 
     klass = self.class
-    code = ''
+    generated_code = ''
     loop do
-      code = klass.generate_random_code(length: 8)
-      break unless klass.exists?(code: code)
+      generated_code = klass.generate_random_code(length: 8)
+      break unless klass.exists?(code: generated_code)
     end
-    self[:code] = code
+    self[:code] = generated_code
   end
 
   def set_default_kitchen

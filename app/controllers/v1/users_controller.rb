f6571796c817725b289c5fcce35f06f292cefc08
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-11-26 00:14:15 +0900

    move to users_controller

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -2,6 +2,8 @@
 
 module V1
   class UsersController < ApplicationController
+    before_action :set_user, only: %i[update]
+
     # api :GET, '/v1/users', 'Show the user'
     # def show
     #   recipe_categories = RecipeCategory.all
@@ -31,9 +33,52 @@ module V1
       end
     end
 
+    api :PUT, '/v1/users', 'User profiles update'
+    def update
+      @user.assign_attributes(user_params)
+      profile = @user.profile
+      profile.assign_attributes(user_profile_params)
+      if @user.save
+        render content_type: 'application/json', json: {
+          message: 'Update completed.'
+        }, status: :ok
+      else
+        errors = @user.errors
+        messages = errors.messages
+        logger.error(messages)
+        # NOTE: ユーザ向けバリデーションエラーを返す
+        message_for_cli = messages.values.flatten.last
+        render content_type: 'application/json', json: {
+          errors: [{
+            code: '400',
+            title: 'Bad request',
+            detail: message_for_cli
+          }]
+        }, status: :bad_request
+      end
+    end
+
+    private
+
+    def set_user
+      # TODO: Fix to `code`
+      @user = User.find(params[:id])
+    end
+
     def user_params
-      params.require(:user).permit(
+      params.permit(
         :email
+      )
+    end
+
+    def user_profile_params
+      params.permit(
+        :user_id,
+        :name,
+        :birth_date,
+        :housework_career,
+        :description,
+        :website_url
       )
     end
   end

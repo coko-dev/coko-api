234a6d053e4921cccb461120e8f2128e291a0b98
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-12-24 15:34:41 +0900

    Fix content type

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,6 +4,6 @@ class ErrorsController < ActionController::API
   rescue_from UnauthorizedError, with: :render_unauthorized
 
   def render_unauthorized
-    render json: { errors: [{ status: '401', title: 'Unauthorized', detail: 'Invalid credentials' }] }, status: :unauthorized
+    render content_type: 'application/json', json: { errors: [{ status: '401', title: 'Unauthorized', detail: 'Invalid credentials' }] }, status: :unauthorized
   end
 end

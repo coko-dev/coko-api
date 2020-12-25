778ec716ef683a4bc031e4c974408b438523002e
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2020-12-25 13:24:50 +0900

    fix to render error msg as json

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,29 @@
 # frozen_string_literal: true
 
 class ErrorsController < ActionController::API
+  rescue_from StandardError, with: :render_server_error
   rescue_from UnauthorizedError, with: :render_unauthorized
 
+  def show
+    raise request.env['action_dispatch.exception']
+  end
+
   def render_unauthorized
-    render content_type: 'application/json', json: { errors: [{ status: '401', title: 'Unauthorized', detail: 'Invalid credentials' }] }, status: :unauthorized
+    render content_type: 'application/json', json: {
+      errors: {
+        status: '401',
+        title: 'Unauthorized',
+        detail: 'Invalid credentials'
+      }
+    }, status: :unauthorized
+  end
+
+  def render_server_error
+    render content_type: 'application/json', json: {
+      errors: {
+        status: '500',
+        title: 'Internal Server Error'
+      }
+    }, status: :internal_server_error
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module CokoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.exceptions_app = ->(env) do
+      ErrorsController.action(:show).call(env)
+    end
   end
 end

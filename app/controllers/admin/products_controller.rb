181662555016e4b56b37b1b17260877476ac0b7d
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-01-19 23:49:59 +0900

    update product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,11 +3,30 @@
 module Admin
   class ProductsController < ApplicationController
     api :POST, '/admin/products', 'Product registration.'
+    param :name, String, require: true, desc: 'Product name'
+    param :name_hira, String, desc: 'Product name of Hiragana'
+    param :product_category_id, String, desc: 'Category id'
     def create
       product = Product.new(product_params)
       product.author = @admin_user
       product.product_category = ProductCategory.find(params[:product_category_id])
       # TODO: Upload image.
+      if product.save
+        render content_type: 'application/json', json: ProductSerializer.new(
+          product
+        ), status: :ok
+      else
+        render_bad_request(object: product)
+      end
+    end
+
+    api :PUT, '/admin/products/:id', 'Product update.'
+    param :name, String, require: true, desc: 'Product name'
+    param :name_hira, String, desc: 'Product name of Hiragana'
+    param :product_category_id, String, desc: 'Category id'
+    def update
+      product = Product.find(params[:id])
+      product.assign_attributes(product_params)
       if product.save
         render content_type: 'application/json', json: ProductSerializer.new(
           product

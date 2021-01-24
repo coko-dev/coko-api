f7f2e1cb8aaf5edaaa1a4c911cb0a835d33fb539
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2021-01-24 22:30:32 +0900

    Create updating kitchen products (#105)
    * Add upadting kitchen products
    
    * Add memo

--- app/controllers/v1/kitchen_products_controller.rb
+++ app/controllers/v1/kitchen_products_controller.rb
@@ -2,8 +2,9 @@
 
 module V1
   class KitchenProductsController < ApplicationController
-    before_action :set_kitchen
-    before_action :verify_current_user_for_kitchen, only: %i[index]
+    before_action :set_kitchen, only: %i[index create]
+    before_action :set_kitchen_product, only: %i[update]
+    before_action :verify_current_user_for_kitchen, only: %i[update]
 
     api :GET, '/v1/kitchen_products', 'Get all products in own kitchen'
     def index
@@ -15,6 +16,9 @@ module V1
     end
 
     api :POST, '/v1/kitchen_products', 'Create a kitchen product'
+    param :product_id, :number, required: true, desc: 'Parent product\'s id'
+    param :note, String, desc: 'User\'s memo'
+    param :best_before, String, desc: 'Ex: \'2021-10-5\' or \'2021-10-05\''
     def create
       product = Product.find(params[:product_id])
       kitchen_product = product.kitchen_products.build(kitchen_product_params)
@@ -30,6 +34,22 @@ module V1
       render_bad_request(e)
     end
 
+    api :PUT, '/v1/kitchen_products/:id', 'Update a kitchen product'
+    param :note, String, desc: 'User\'s memo'
+    param :best_before, String, desc: 'Ex: \'2021-10-5\' or \'2021-10-05\''
+    def update
+      @kitchen_product.assign_attributes(kitchen_product_params)
+      # NOTE: When building with params, no error occurs and it becomes nil.
+      @kitchen_product.best_before = params[:best_before].to_date
+      @kitchen_product.save!
+      render content_type: 'application/json', json: KitchenProductSerializer.new(
+        @kitchen_product,
+        include: associations_for_serialization
+      ), status: :ok
+    rescue StandardError => e
+      render_bad_request(e)
+    end
+
     private
 
     def associations_for_serialization
@@ -38,6 +58,7 @@ module V1
       ]
     end
 
+    # NOTE: Don't include 'best_before' in params to make an error if it cannot be converted to date type.
     def kitchen_product_params
       params.permit(
         %i[
@@ -50,8 +71,13 @@ module V1
       @kitchen = @current_user.kitchen
     end
 
+    def set_kitchen_product
+      @kitchen_product = KitchenProduct.find(params[:id])
+    end
+
     def verify_current_user_for_kitchen
-      return if @current_user.my_kitchen?(@kitchen)
+      kitchen = @kitchen_product.kitchen
+      return if @current_user.my_kitchen?(kitchen)
 
       raise ForbiddenError
     end

f4e4fb0ae657be1cdae8c1c7842d09dc5e740c3c
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-01-29 12:54:38 +0900

    Add ja.yml

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -6,11 +6,11 @@ class UserProfile < ApplicationRecord
 
   before_validation :set_default_display_id, on: :create
 
-  validates :display_id, presence: { message: 'ユーザ名を入力してください' }, uniqueness: { case_sensitive: { message: 'このユーザ名は既に存在しています' } }, format: { with: /\A[0-9a-zA-Z]+\z/, message: '半角英数文字のみが使えます' }, on: %i[create update]
-  validates :name, presence: { message: 'ニックネーム入力は必須です' }, length: { in: 2..25, message: 'ニックネーム2~25文字で入力してください' }, on: %i[update]
-  validates :housework_career, length: { maximum: 3, message: '本当にそんな生きてるの..?' }, numericality: { only_integer: true, message: '家事歴年数は半角数字で入力してください' }, allow_nil: true, on: %i[update]
-  validates :description, length: { maximum: 120, message: '紹介文は%<count>s文字以内で入力してください' }, on: %i[update]
-  validates :website_url, length: { maximum: 100, message: '外部リンクは%<count>s文字以内で入力してください' }, on: %i[update]
+  validates :display_id, presence: true, uniqueness: { case_sensitive: true }, format: { with: /\A[0-9a-zA-Z]+\z/, message: '半角英数文字のみが使えます' }, on: %i[create update]
+  validates :name, presence: true, length: { in: 2..25 }, on: %i[update]
+  validates :housework_career, length: { maximum: 3 }, numericality: { only_integer: true }, allow_nil: true, on: %i[update]
+  validates :description, length: { maximum: 120 }, on: %i[update]
+  validates :website_url, length: { maximum: 100 }, on: %i[update]
 
   belongs_to :user
 

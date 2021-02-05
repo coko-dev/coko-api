72ed0ed15c83c7496dd5a45db28814494fd3938c
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-02-05 12:14:56 +0900

    Fix rc controllers

--- app/controllers/admin/recipe_categories_controller.rb
+++ app/controllers/admin/recipe_categories_controller.rb
@@ -2,28 +2,51 @@
 
 module Admin
   class RecipeCategoriesController < ApplicationController
-    api :GET, '/admin/recipe_categories', 'Show the recipe category'
+    before_action :set_recipe_category, only: %i[show]
+
+    api :GET, '/admin/recipe_categories', 'Show all recipe category'
     def index
-      recipe_categories = RecipeCategory.all
-      render json: { recipe_categories: recipe_categories }
+      render content_type: 'application/json', json: RecipeCategorySerializer.new(
+        RecipeCategory.all
+      )
+    rescue StandardError => e
+      render_bad_request(e)
     end
 
-    api :POST, '/admin/recipe_categories', 'Create recipe categories'
+    api :GET, '/admin/recipe_categories/:id', 'Show a recipe category'
+    def show
+      render content_type: 'application/json', json: RecipeCategorySerializer.new(
+        @recipe_category
+        # TODO: include related recipes
+      )
+    rescue StandardError => e
+      render_bad_request(e)
+    end
+
+    api :POST, '/admin/recipe_categories', 'Create a recipe category'
     def create
       recipe_category = RecipeCategory.new(recipe_category_params)
-      if recipe_category.save
-        render json: { status: 'SUCCESS', data: recipe_category }
-      else
-        render json: { status: 'ERROR', data: recipe_category.error }
-      end
+      # NOTE: Use '.find_by' as parent_category_id can be null.
+      recipe_category.parent_category = RecipeCategory.find_by(id: params[:parent_category_id])
+      recipe_category.save!
+      render content_type: 'application/json', json: RecipeCategorySerializer.new(
+        recipe_category
+      )
+    rescue StandardError => e
+      render_bad_request(e)
+    end
+
+    private
+
+    def set_recipe_category
+      @recipe_category = RecipeCategory.find(params[:id])
     end
 
     def recipe_category_params
-      params.require(:recipe_category).permit(
+      params.permit(
         %i[
           name
           name_slug
-          recipe_category_id
         ]
       )
     end
--- app/models/recipe_category.rb
+++ app/models/recipe_category.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RecipeCategory < ApplicationRecord
-  belongs_to :parent_category, class_name: 'RecipeCategory', optional: true
+  belongs_to :parent_category, foreign_key: 'recipe_category_id_from', class_name: 'RecipeCategory', inverse_of: 'child_categories', optional: true
 
   has_many :child_categories, foreign_key: 'recipe_category_id_from', class_name: 'RecipeCategory', inverse_of: 'parent_category', dependent: :nullify
   has_many :recipes, dependent: :nullify

a2122fc594441b6e5298d59cc79e6cb6491a116f
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2021-02-05 12:17:38 +0900

    Fix rc controllers (#122)

---
+++ app/serializers/recipe_category_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class RecipeCategorySerializer < ApplicationSerializer
+  attribute :name, :name_slug
+
+  belongs_to :parent_category do |object|
+    object&.parent_category
+  end
+
+  has_many :child_categories do |object|
+    object&.child_categories
+  end
+end

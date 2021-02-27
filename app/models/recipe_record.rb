610574fee48744a538d524fcedf4d20014486fb8
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-02-27 19:43:42 +0900

    Regist recipe record

--- app/models/recipe_record.rb
+++ app/models/recipe_record.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class RecipeRecord < ApplicationRecord
+  validates :body, presence: true, length: { maximum: 400 }
+
   belongs_to :author, class_name: 'User', inverse_of: 'recipe_records'
   belongs_to :recipe
 
   has_many :recipe_record_images, dependent: :delete_all
+
+  def build_each_images(image_params)
+    return if image_params.blank?
+
+    image_params.each do |param|
+      recipe_record_images.build(image: param[:image])
+    end
+  end
 end

06ef397f7e4ac20ec36bc97e6374b8a61ae78326
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-02-28 17:24:27 +0900

    Showing lists

--- app/controllers/v1/kitchen_shopping_lists_controller.rb
+++ app/controllers/v1/kitchen_shopping_lists_controller.rb
@@ -2,6 +2,15 @@
 
 module V1
   class KitchenShoppingListsController < ApplicationController
+    api :GET, '/v1/kitchen_shopping_lists', 'Show shopping lists'
+    def index
+      ksls = @current_user.kitchen.kitchen_shopping_lists
+      render content_type: 'application/json', json: KitchenShoppingListSerializer.new(
+        ksls,
+        include: association_for_lists
+      ), status: :ok
+    end
+
     api :POST, '/v1/kitchen_shopping_lists', 'Create shopping lists'
     param :kitchen_shopping_lists, Array, required: true, desc: 'Shopping lists' do
       param :product_id, :number, required: true, desc: "Parent product's id"
@@ -15,6 +24,13 @@ module V1
     end
 
     private
+
+    def association_for_lists
+      %i[
+        product
+        user
+      ]
+    end
 
     def shopping_list_params
       params.permit(

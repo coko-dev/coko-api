77fb48f03ba02cb7a9b3891a2de3ed557d947ecb
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2021-02-28 17:31:33 +0900

    Create show shopping lists (#152)
    * Showing lists
    
    * Fix ja.yml

--- app/controllers/v1/kitchen_shopping_lists_controller.rb
+++ app/controllers/v1/kitchen_shopping_lists_controller.rb
@@ -2,6 +2,15 @@
 
 module V1
   class KitchenShoppingListsController < ApplicationController
+    api :GET, '/v1/kitchen_shopping_lists', 'Show shopping lists'
+    def index
+      ksls = @current_user.kitchen.kitchen_shopping_lists
+      render content_type: 'application/json', json: KitchenShoppingListSerializer.new(
+        ksls,
+        include: association_for_lists
+      ), status: :ok
+    end
+
     api :POST, '/v1/kitchen_shopping_lists', 'Create shopping lists'
     param :kitchen_shopping_lists, Array, required: true, desc: 'Shopping lists' do
       param :product_id, :number, required: true, desc: "Parent product's id"
@@ -12,9 +21,22 @@ module V1
       kitchen = @current_user.kitchen
       ksls.each { |ksl| ksl.kitchen = kitchen }
       @current_user.save!
+      render content_type: 'application/json', json: KitchenShoppingListSerializer.new(
+        ksls,
+        include: association_for_lists
+      ), status: :ok
+    rescue StandardError => e
+      render_bad_request(e)
     end
 
     private
+
+    def association_for_lists
+      %i[
+        product
+        user
+      ]
+    end
 
     def shopping_list_params
       params.permit(

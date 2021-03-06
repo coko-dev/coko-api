9c152ae6a0cd4b170ad46bbfe5218f5d1ab8ee37
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-03-06 14:49:44 +0900

    Update recipe and product categories

--- app/controllers/admin/recipe_categories_controller.rb
+++ app/controllers/admin/recipe_categories_controller.rb
@@ -2,11 +2,13 @@
 
 module Admin
   class RecipeCategoriesController < ApplicationController
+    before_action :set_recipe_category, only: %i[update]
+
     api :GET, '/admin/recipe_categories', 'Show all recipe category'
     def index
       render content_type: 'application/json', json: RecipeCategorySerializer.new(
         RecipeCategory.all
-      )
+      ), status: :ok
     rescue StandardError => e
       render_bad_request(e)
     end
@@ -22,12 +24,32 @@ module Admin
       recipe_category.save!
       render content_type: 'application/json', json: RecipeCategorySerializer.new(
         recipe_category
-      )
+      ), status: :ok
+    rescue StandardError => e
+      render_bad_request(e)
+    end
+
+    api :PUT, '/admin/recipe_categories', 'Update recipe category'
+    param :name, String, allow_blank: true, desc: 'Category name for display'
+    param :name_slug, String, allow_blank: true, desc: 'Category name slug'
+    param :parent_category_id, :number, allow_blank: true, desc: "Parent recipe's key"
+    def update
+      @recipe_category.assign_attributes(recipe_category_params)
+      parent_category_id = params[:parent_category_id]
+      @recipe_category.parent_category = RecipeCategory.find(parent_category_id) if parent_category_id.present?
+      @recipe_category.save!
+      render content_type: 'application/json', json: RecipeCategorySerializer.new(
+        @recipe_category
+      ), status: :ok
     rescue StandardError => e
       render_bad_request(e)
     end
 
     private
+
+    def set_recipe_category
+      @recipe_category = RecipeCategory.find(params[:id])
+    end
 
     def recipe_category_params
       params.permit(

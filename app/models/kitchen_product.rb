cd538886100ab7a01e8a3bc1dfaef2b9c748adfb
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2021-05-17 12:01:00 +0900

    Add attribute 'added_on' (#195)

--- app/models/kitchen_product.rb
+++ app/models/kitchen_product.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class KitchenProduct < ApplicationRecord
+  before_create :set_default_added_on
+
   belongs_to :kitchen
   belongs_to :product
+
+  def set_default_added_on
+    self.added_on ||= Time.zone.today
+  end
 end

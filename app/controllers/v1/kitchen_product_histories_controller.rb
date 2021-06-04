7050cfa5944716d4b4ebe3eeff4442ef0b621050
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-06-04 14:13:53 +0900

    Fix api note

--- app/controllers/v1/kitchen_product_histories_controller.rb
+++ app/controllers/v1/kitchen_product_histories_controller.rb
@@ -2,7 +2,7 @@
 
 module V1
   class KitchenProductHistoriesController < ApplicationController
-    api :GET, '/v1/kitchen_product_histories', 'Show all product logs in own kitchen'
+    api :GET, '/v1/kitchen_product_histories', 'Show last 30 product logs in own kitchen'
     def index
       histories = @current_user.kitchen.kitchen_product_histories.order(date: :desc, updated_at: :desc).limit(30)
       render content_type: 'application/json', json: KitchenProductHistorySerializer.new(

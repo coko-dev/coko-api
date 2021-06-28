717e721fdd8047010b9db8512269f4593fdc3f27
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-06-28 23:41:54 +0900

    Add counter for display

--- app/models/concerns/string_util.rb
+++ app/models/concerns/string_util.rb
@@ -14,5 +14,28 @@ module StringUtil
       limit = 10**length
       SecureRandom.random_number(limit)
     end
+
+    def number_for_display(number)
+      digits = { 100_000_000 => '億', 10_000 => '万' }.freeze
+      display_num = number
+      digit = ''
+
+      digits.each do |n, d|
+        next if number < n
+
+        display_num = (number.to_f / n).ceil(1)
+        digit = d
+        break
+      end
+
+      display_num =
+        if display_num >= 1_000
+          display_num.ceil.to_s(:delimited)
+        else
+          display_num.to_s
+        end
+
+      "#{display_num}#{digit}"
+    end
   end
 end

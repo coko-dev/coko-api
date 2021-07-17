80651be25ce74b1c71c5215b39ce96fd691f3fb2
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-07-17 22:24:09 +0900

    Fix user follow param

--- app/controllers/v1/user_follows_controller.rb
+++ app/controllers/v1/user_follows_controller.rb
@@ -4,21 +4,21 @@ module V1
   class UserFollowsController < ApplicationController
     before_action :set_user
 
-    api :GET, '/v1/users/:code/followers', 'Show user followers'
+    api :GET, '/v1/users/:display_id/followers', 'Show user followers'
     def show_followers
       render content_type: 'application/json', json: UserSerializer.new(
         @user.follower_users
       ), status: :ok
     end
 
-    api :GET, '/v1/users/:code/followings', 'Show user followings'
+    api :GET, '/v1/users/:display_id/followings', 'Show user followings'
     def show_followings
       render content_type: 'application/json', json: UserSerializer.new(
         @user.following_users
       ), status: :ok
     end
 
-    api :POST, '/v1/users/:code/follow', 'Follow a user'
+    api :POST, '/v1/users/:display_id/follow', 'Follow a user'
     def create
       is_new_record = @current_user.follow(@user)
       render content_type: 'application/json', json: {
@@ -26,7 +26,7 @@ module V1
       }, status: :ok
     end
 
-    api :DELETE, '/v1/users/:code/unfollow', 'Unfollow a user'
+    api :DELETE, '/v1/users/:display_id/unfollow', 'Unfollow a user'
     def destroy
       is_deleted = @current_user.unfollow(@user)
       render content_type: 'application/json', json: {
@@ -37,7 +37,7 @@ module V1
     private
 
     def set_user
-      @user = User.find_by!(code: params[:code])
+      @user = User.joins(:profile).find_by!(user_profiles: { display_id: params[:display_id] })
     end
   end
 end

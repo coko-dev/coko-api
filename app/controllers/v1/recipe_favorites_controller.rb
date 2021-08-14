57617381ef80a9210a2483a38ad7512b3b177f7e
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-08-14 15:12:52 +0900

    Make favorite resource

--- app/controllers/v1/recipe_favorites_controller.rb
+++ app/controllers/v1/recipe_favorites_controller.rb
@@ -4,7 +4,7 @@ module V1
   class RecipeFavoritesController < ApplicationController
     before_action :set_recipe
 
-    api :POST, '/v1/recipes/:id/recipe_favorites', 'Make a recipe favorite'
+    api :POST, '/v1/recipes/:id/recipe_favorite', 'Make a recipe favorite'
     def create
       is_new_favorite = RecipeFavorite.where(recipe: @recipe, user: @current_user).empty?
       if is_new_favorite
@@ -16,7 +16,7 @@ module V1
       }, status: :ok
     end
 
-    api :DELETE, '/v1/recipes/:id/recipe_favorites', 'Delete a recipe favorite'
+    api :DELETE, '/v1/recipes/:id/recipe_favorite', 'Delete a recipe favorite'
     def destroy
       favorite = RecipeFavorite.find_by(recipe: @recipe, user: @current_user)
       is_exists = favorite.present?

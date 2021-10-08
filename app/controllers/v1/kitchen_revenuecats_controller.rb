b771839cd7eeea56c556f4f481560edc41182f4b
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-10-08 23:50:00 +0900

    Add deleting subscription status

--- app/controllers/v1/kitchen_revenuecats_controller.rb
+++ app/controllers/v1/kitchen_revenuecats_controller.rb
@@ -2,20 +2,38 @@
 
 module V1
   class KitchenRevenuecatsController < ApplicationController
-    api :POST, 'v1/kitchen_revenuecat', 'Create revenuecat for self kitchen'
-    def create
-      kitchen = @current_user.kitchen
-      raise StandardError, 'Not subscribed' unless RevenuecatClient.subscribed?(type: :kitchen, id: kitchen.id)
+    before_action :set_kitchen
 
-      kitchen.is_subscriber = true
-      is_changed = kitchen.changed?
-      kitchen.save!
+    api :POST, 'v1/kitchen_revenuecat', 'Create subscription status'
+    def create
+      raise StandardError, 'Not subscribed' unless RevenuecatClient.subscribed?(type: :kitchen, id: @kitchen.id)
+
+      @kitchen.update!(is_subscriber: true)
 
       render content_type: 'application/json', json: {
-        data: { meta: { is_new_subscriber: is_changed } }
+        data: { meta: { is_new_subscriber: @kitchen.saved_changes? } }
       }, status: :ok
     rescue StandardError => e
       render_bad_request(e)
+    end
+
+    api :DELETE, 'v1/kitchen_revenuecat', 'Delete subscription status'
+    def destroy
+      raise StandardError, 'Did not delete subscription status. You are currently subscribed' if RevenuecatClient.subscribed?(type: :kitchen, id: @kitchen.id)
+
+      @kitchen.update!(is_subscriber: false)
+
+      render content_type: 'application/json', json: {
+        data: { meta: { is_deleted: @kitchen.saved_changes? } }
+      }, status: :ok
+    rescue StandardError => e
+      render_bad_request(e)
+    end
+
+    private
+
+    def set_kitchen
+      @kitchen = @current_user.kitchen
     end
   end
 end

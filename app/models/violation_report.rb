e4ec462c394a4cea6d7d950bac6adff905ba7e23
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-10-16 00:35:19 +0900

    Fix for linter

--- app/models/violation_report.rb
+++ app/models/violation_report.rb
@@ -4,6 +4,6 @@ class ViolationReport < ApplicationRecord
   validates :reason, length: { maximum: 120 }
   validates :description, length: { maximum: 120 }
 
-  belongs_to :reporting_user, foreign_key: 'reporting_user_id', class_name: 'User', inverse_of: :reportings
-  belongs_to :reported_user, foreign_key: 'reported_user_id', class_name: 'User', inverse_of: :reporteds
+  belongs_to :reporting_user, class_name: 'User', inverse_of: :reportings
+  belongs_to :reported_user, class_name: 'User', inverse_of: :reporteds
 end

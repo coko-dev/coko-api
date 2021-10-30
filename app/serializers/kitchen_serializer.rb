237b7acc60db043da9c58faa34657a89d46f081d
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-10-30 14:28:09 +0900

    unlimit ocr count for subscriber

--- app/serializers/kitchen_serializer.rb
+++ app/serializers/kitchen_serializer.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class KitchenSerializer < ApplicationSerializer
+  # TODO: last_action_at, todays_ocr_count は自身のキッチンの場合のみ表示
   attribute :name, :is_subscriber, :owner_user_id, :last_action_at
+
+  attribute :todays_ocr_count do |object|
+    object.todays_ocr_count
+  end
 end

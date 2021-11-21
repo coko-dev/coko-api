2ca6841f42aa363ab1f45f3f005afef03fd18a82
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-11-21 15:49:14 +0900

    Use docker compose for run ridgepole

--- .run_ridgepole.sh
+++ .run_ridgepole.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ]; then
-  docker-compose exec api ridgepole --apply -c config/database.yml -f db/Schemafile.rb
+  docker compose run api ridgepole --apply -c config/database.yml -f db/Schemafile.rb
 elif [ "$1" = "prd" ]; then
   heroku run ridgepole --apply -c config/database.yml -f db/Schemafile.rb -E production
 else

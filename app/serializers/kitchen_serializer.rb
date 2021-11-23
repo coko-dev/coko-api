8017ff4a867b73f10b73af5de2e100c65ed4c80e
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-11-23 19:50:40 +0900

    Use &:users

--- app/serializers/kitchen_serializer.rb
+++ app/serializers/kitchen_serializer.rb
@@ -5,7 +5,5 @@ class KitchenSerializer < ApplicationSerializer
 
   attributes :is_subscriber, :last_action_at, :todays_ocr_count, if: proc { |record, params| params[:current_user]&.my_kitchen?(record) }
 
-  has_many :users, id_method_name: :code do |object|
-    object.users
-  end
+  has_many :users, id_method_name: :code, &:users
 end

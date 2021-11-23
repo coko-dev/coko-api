93c0516692c69cdc446ebcf321f428716db5ee11
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-11-24 00:08:58 +0900

    Add basic auth

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Apipie.configure do |config|
-  config.app_name     = 'CokoApi'
+  config.app_name = 'CokoApi'
   config.api_base_url = ''
   config.doc_base_url = '/apidoc'
-  # where is your API defined?
-  config.api_controllers_matcher = Rails.root.join('/app/controllers/**/*.rb')
+  config.api_controllers_matcher = Rails.root.join('app/controllers/**/*.rb')
   config.api_routes = Rails.application.routes
   config.translate = false
+  config.authenticate = proc {
+    basic_auth = Rails.application.credentials[:basic_auth]
+    authenticate_or_request_with_http_basic do |username, password|
+      username == basic_auth[:user_name] && password == basic_auth[:password]
+    end
+  }
 end

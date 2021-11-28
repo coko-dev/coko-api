26a517beb0988aa80124439cf49fda2cd2765545
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2021-11-28 18:35:55 +0900

    Fix limit for product registration

--- app/models/kitchen_product.rb
+++ app/models/kitchen_product.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class KitchenProduct < ApplicationRecord
+  MAXIMUM_NUMBER = 30
+
   before_create :set_default_added_on
 
   belongs_to :kitchen
@@ -8,5 +10,12 @@ class KitchenProduct < ApplicationRecord
 
   def set_default_added_on
     self.added_on ||= Time.zone.today
+  end
+
+  class << self
+    def over_num_limit?(kitchen, add_num: nil)
+      add_num ||= 0
+      kitchen.kitchen_products.count + add_num > MAXIMUM_NUMBER
+    end
   end
 end

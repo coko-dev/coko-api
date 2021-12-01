14705224daa2fe1c9e3092fcad25404ddb7e3e31
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2021-12-01 13:04:00 +0900

    Fix serializer foreign key (#325)
    * Add belongs_to to product serializer
    
    * Add belongs_to for kitchen owner

--- app/serializers/kitchen_serializer.rb
+++ app/serializers/kitchen_serializer.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class KitchenSerializer < ApplicationSerializer
-  attributes :name, :owner_user_id
+  attributes :name
 
   attributes :is_subscriber, :last_action_at, :todays_ocr_count, if: proc { |record, params| params[:current_user]&.my_kitchen?(record) }
 
+  belongs_to :owner, serializer: UserSerializer, id_method_name: :owner_code
   has_many :users, id_method_name: :code, &:users
 end

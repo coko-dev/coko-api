4146e5922d64a28fed092eac90ae9b8a9e969ab3
Taiki Tsuboya <tsuboyataiki.427dtk@gmail.com>
2021-12-30 21:46:23 +0900

    Migrate id to string (#361)
    * Remove fkey
    
    * Return fkey
    
    * migrate id to string
    
    * Add default string id
    
    * Fix for linter
    
    * Remove fk disable

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,18 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  include StringUtil
+
+  before_create :set_id
+
+  def set_id
+    return if self[:id].present?
+
+    klass = self.class
+    self[:id] = loop do
+      generated_id = klass.generate_random_code(length: 12)
+      break generated_id unless klass.exists?(id: generated_id)
+    end
+  end
 end

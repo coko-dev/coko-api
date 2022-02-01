9d522c2f9b92cccb5d742df1537e722860ab0c44
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2022-02-01 14:59:24 +0900

    Add authenticate with firebase id_token

--- app/models/concerns/auth_util.rb
+++ app/models/concerns/auth_util.rb
@@ -9,6 +9,8 @@ module AuthUtil
   JWT_DEFAULT_ALGORITHM = 'HS256'
   JWT_FIREBASE_ALGORITHM = 'RS256'
   JWT_FIREBASE_AUDIENCE = 'https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit'
+  JWT_FIREBASE_PUBLIC_KEYS_URL = 'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com'
+  JWT_FIREBASE_ISSUER = 'https://securetoken.google.com/prd-coko'
 
   module ClassMethods
     def jwt_encode_for_general(subject: nil, type: 'admin_user', expire: 30.days)
@@ -56,11 +58,26 @@ module AuthUtil
     end
 
     def jwt_decode_for_firebase(encoded_token)
-      JWT.decode(encoded_token, firebase_private_key, true, algorithm: JWT_FIREBASE_ALGORITHM).first.symbolize_keys
+      unverified_token_payload, unverified_token_header = jwt_decode_without_vetification(encoded_token)
+      public_key =
+        if unverified_token_payload['iss'] == JWT_FIREBASE_ISSUER
+          OpenSSL::X509::Certificate.new(firebase_public_keys[unverified_token_header['kid']]).public_key
+        else
+          firebase_private_key.public_key
+        end
+      JWT.decode(encoded_token, public_key, false, algorithm: JWT_FIREBASE_ALGORITHM).first.symbolize_keys
+    end
+
+    def jwt_decode_without_vetification(token)
+      JWT.decode(token, nil, false)
     end
 
     def firebase_credentials
       Rails.application.credentials.firebase[:credentials]
+    end
+
+    def firebase_public_keys
+      JSON.parse(Typhoeus.get(JWT_FIREBASE_PUBLIC_KEYS_URL).body)
     end
 
     def firebase_private_key

7acecf18740d0034192017f51a8f040133a3bd4d
kawano <daisukekawano67@gmail.com>
2022-08-12 09:08:03 +0900

    fix mistakes

--- db/Schemafile.rb
+++ db/Schemafile.rb
@@ -369,16 +369,16 @@ end
 add_index 'active_storage_blobs', %w[key], name: 'idx_active_storage_blobs_on_key', unique: true
 
 create_table 'active_storage_attachments', force: :cascade do |t|
-  t.string     :name,     null: false
-  t.references :record,   null: false, polymorphic: true, index: false
-  t.references :blob,     null: false
+  t.string     :name,       null: false
+  t.references :record,     null: false, polymorphic: true, index: false
+  t.references :blob,       null: false
   t.datetime   :created_at, null: false
 end
 add_index       'active_storage_attachments', %w[record_type record_id blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
 add_foreign_key 'active_storage_attachments', 'active_storage_blobs',            name: 'fk_active_storage_attachments_1', column: 'blob_id'
 
 create_table 'active_storage_variant_records', force: :cascade do |t|
-  t.belongs_to :blob, null: false, index: false
+  t.belongs_to :blob,             null: false, index: false
   t.string     :variation_digest, null: false
 end
 add_index       'active_storage_variant_records', %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
